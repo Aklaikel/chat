a894fb794db93eb103c979879f9f372f6f20f546
Aklaikel <anas.klaikel95@gmail.com>
2024-01-04 02:58:25 +0100

    changing to blue

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
   return (
     <div className="container mx-auto max-w-[700px]">
       <div className="flex flex-col h-screen bg-gray-900">
-        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">DR. Vi</h1>
+        <h1 className="bg-gradient-to-r from-blue-500 to-blue-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">DR. Vi</h1>
         <div className="flex-grow p-6">
           <div className="flex flex-col space-y-4">
           {
@@ -75,7 +75,7 @@ export default function Home() {
             message.type === 'user' ? 'justify-end' : 'justify-start'
             }`}>
             <div className={`${
-              message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
+              message.type === 'user' ? 'bg-blue-500' : 'bg-gray-800'
             } rounded-lg p-4 text-white max-w-sm`}>
             {message.message}
             </div>
@@ -95,7 +95,7 @@ export default function Home() {
         <form onSubmit={handleSubmit} className="flex-none p-6">
           <div className="flex rounded-lg border border-gray-700 bg-gray-800">  
         <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
+            <button type="submit" className="bg-blue-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-blue-600 transition-colors duration-300">Send</button>
             </div>
         </form>
         </div>
